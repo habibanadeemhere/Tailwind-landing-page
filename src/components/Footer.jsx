a826e343368a2b3a90a34bf17a56f253505e7de5
Habiba Nadeem <habibanadeem075@gmail.com>
2026-03-07 23:51:47 -0800

    text-left added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,9 +3,9 @@ import Map from '../assets/map.png'
 const Footer = () => {
   return (
     <>
-   <section className="footer w-full">
+   <section className="footer w-full text-left">
 
-<div className="foot flex flex-col md:flex-row flex-wrap justify-between mt-5 items-start gap-10 md:gap-24 text-[#787878] bg-[#232323] px-6 md:px-16 py-10">
+<div className="foot flex flex-col md:flex-row flex-wrap justify-between mt-5 items-start gap-10 md:gap-24 text-[#787878] bg-[#232323] px-6 md:px-16 py-10 text-left">
 
 
   <div className="sec1 flex flex-col items-center md:items-start mt-10 md:mt-6 bg-cover" >
