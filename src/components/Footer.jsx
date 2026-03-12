43fa5159b967594d64e5670b428a1309e4e8d72f
Habiba <habibanadeem075@gmail.com>
2026-03-12 13:52:30 +0500

    Add margin-top to footer section
    Added margin-top to the footer section.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import Map from '../assets/map.png'
 
 const Footer = () => {
   return (
-    <section className="w-screen bg-[#232323] relative left-1/2 right-1/2 -ml-[50vw] -mr-[50vw]">
+    <section className="w-screen bg-[#232323] relative left-1/2 right-1/2 -ml-[50vw] -mr-[50vw] mt-5">
       <div className="w-full max-w-screen-2xl mx-auto flex flex-col md:flex-row justify-between items-start gap-10 text-[#787878] px-10 md:px-24 py-12">
         <div className="flex flex-col items-start">
           <img src={Map} alt="Map" className="w-32 md:w-40"/>
