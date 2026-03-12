801328843b9d2bac740a65a9f8c01ffa7c25fcc1
Habiba <habibanadeem075@gmail.com>
2026-03-12 13:50:01 +0500

    Refactor Footer component layout for better styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,16 +5,13 @@ const Footer = () => {
   return (
     <>
       <section className="footer w-full bg-[#232323]">
-
-        <div className="foot w-full flex flex-col md:flex-row flex-wrap justify-between items-start gap-10 md:gap-24 text-[#787878] px-10 md:px-24 py-12">
-
+        <div className="foot w-full flex flex-col md:flex-row justify-between items-start gap-10 text-[#787878] px-10 md:px-24 py-12">
           <div className="sec1 flex flex-col items-start">
             <img src={Map} alt="Map" className="w-32 md:w-40"/>
             <p className="mt-3 text-sm">
               Made with lots of ❤️ in San Francisco
             </p>
           </div>
-
           <div className="sec2">
             <h5 className="text-white font-bold mb-2">Products</h5>
             <ul className="leading-6 text-sm space-y-1">
@@ -25,7 +22,6 @@ const Footer = () => {
               <li>Acumen</li>
             </ul>
           </div>
-
           <div className="sec3">
             <h5 className="text-white font-bold mb-2">Resources</h5>
             <ul className="leading-6 text-sm space-y-1">
@@ -37,7 +33,6 @@ const Footer = () => {
               <li>Quiz Maker</li>
             </ul>
           </div>
-
           <div className="sec4">
             <h5 className="text-white font-bold mb-2">Company</h5>
             <ul className="leading-6 text-sm space-y-1">
@@ -47,9 +42,7 @@ const Footer = () => {
               <li>Contact Us</li>
             </ul>
           </div>
-
         </div>
-
       </section>
     </>
   )
