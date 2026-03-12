7a4cff9539878eab227692e3d972a8735a7d118e
Habiba <habibanadeem075@gmail.com>
2026-03-12 13:48:57 +0500

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,19 +4,17 @@ import Map from '../assets/map.png'
 const Footer = () => {
   return (
     <>
-      <section className="footer w-full">
+      <section className="footer w-full bg-[#232323]">
 
-        <div className="foot w-full max-w-[1200px] mx-auto flex flex-col md:flex-row flex-wrap justify-between items-start gap-10 md:gap-24 text-[#787878] bg-[#232323] px-6 md:px-16 py-10">
+        <div className="foot w-full flex flex-col md:flex-row flex-wrap justify-between items-start gap-10 md:gap-24 text-[#787878] px-10 md:px-24 py-12">
 
-     
-          <div className="sec1 relative flex flex-col items-start mt-6">
-            <img src={Map} alt="Map" className="w-32 md:w-40" />
+          <div className="sec1 flex flex-col items-start">
+            <img src={Map} alt="Map" className="w-32 md:w-40"/>
             <p className="mt-3 text-sm">
-              Made with lots of ❤️ in San Francisco.
+              Made with lots of ❤️ in San Francisco
             </p>
           </div>
 
-         
           <div className="sec2">
             <h5 className="text-white font-bold mb-2">Products</h5>
             <ul className="leading-6 text-sm space-y-1">
@@ -28,7 +26,6 @@ const Footer = () => {
             </ul>
           </div>
 
-         
           <div className="sec3">
             <h5 className="text-white font-bold mb-2">Resources</h5>
             <ul className="leading-6 text-sm space-y-1">
@@ -41,7 +38,6 @@ const Footer = () => {
             </ul>
           </div>
 
-         
           <div className="sec4">
             <h5 className="text-white font-bold mb-2">Company</h5>
             <ul className="leading-6 text-sm space-y-1">
