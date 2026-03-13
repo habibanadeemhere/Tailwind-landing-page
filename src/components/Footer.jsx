89695572729cbda354214c581130b0ae694c22d1
Habiba <habibanadeem075@gmail.com>
2026-03-13 13:15:30 +0500

    Center footer text in Footer component
    Updated the positioning of the footer text to center it vertically and horizontally.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,12 +6,12 @@ const Footer = () => {
     <section className="w-full bg-[#232323]">
       <div className="w-full max-w-screen-2xl mx-auto flex flex-col md:flex-row justify-between items-start gap-10 text-[#787878] px-10 md:px-24 py-12 mt-5">
         
-        <div className="flex flex-col items-center relative">
-          <img src={Map} alt="Map" className="w-32 md:w-40"/>
-          <p className="absolute bottom-0 text-center text-xs w-full">
-            Made with lots of ❤️ in San Francisco
-          </p>
-        </div>
+       <div className="flex flex-col items-center relative">
+  <img src={Map} alt="Map" className="w-32 md:w-40"/>
+  <p className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-center text-xs whitespace-nowrap">
+    Made with lots of ❤️ in San Francisco
+  </p>
+</div>
 
         <div>
           <h5 className="text-white font-bold mb-2">Products</h5>
