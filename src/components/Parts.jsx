bfb356e954f6b81ef75a3d310122e70139e2bc6a
Habiba <habibanadeem075@gmail.com>
2026-03-13 13:21:26 +0500

    Refactor Parts component layout and content
    Updated layout and text formatting in Parts component.

--- src/components/Parts.jsx
+++ src/components/Parts.jsx
@@ -3,8 +3,8 @@ import image from '../assets/image 40.png'
 
 const Parts = () => {
   return (
-    <section className="flex gap-4 py-12 justify-center items-center flex-col md:flex-row px-6 md:px-10">
-      <div className="text-left w-full md:w-1/2">
+    <section className="flex gap-16 py-12 justify-center items-center flex-col md:flex-row px-6">
+      <div className="text-left">
         <button className="bg-[#EFFEFF] border rounded px-2 py-1">
           OUR PROMISES
         </button>
@@ -12,17 +12,14 @@ const Parts = () => {
           Tool <b>built for people.</b>
         </h2>
         <p className="mt-4 text-[#676767]">
-          Whether you want to edit your Google Docs,
-          resolve Jira issues, or collaborate over Zoom.
-          <br /><br />
-          Circle has 100+ integrations with tools you
-          already use and love.
+          Whether you want to edit your Google Docs,<br /> resolve Jira issues, or collaborate over Zoom. <br /><br />
+          Circle has 100+ integrations with tools you <br /> already use and love.
         </p>
         <button className="mt-4 bg-[#0CBBC7] text-white px-6 py-3 rounded">
           Get started free
         </button>
       </div>
-      <div className="w-full md:w-1/2 flex justify-center">
+      <div>
         <img src={image} className="rounded w-full max-w-[400px]" alt="" />
       </div>
     </section>
